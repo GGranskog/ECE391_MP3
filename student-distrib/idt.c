696e8b004a4e287732f133faf3dafe165da4a1f9
georgeg5 <georgeg5@illinois.edu>
2023-10-12 18:12:15 -0500

    idt initialization test

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -4,5 +4,20 @@
 void
 init_idt()
 {
-    
+    int i;
+
+    for(i = 0; i < NUM_VEC; i++)
+    {
+        
+        idt[i].reserved0 = 0;
+        idt[i].reserved1 = 0;
+        idt[i].reserved2 = 0;
+        idt[i].reserved3 = 0;
+        idt[i].reserved4 = 0;
+
+        idt[i].size = 1;
+        idt[i].seg_selector = KERNEL_CS;
+        idt[i].dpl = 0;
+        idt[i].present = 0;
+    }
 }
