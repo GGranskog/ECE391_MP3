9c0001b5d3e0e5b3fc4a732a7df727f0b86b1122
ankits6 <ankits6@illinois.edu>
2023-10-16 03:24:29 -0500

    Update file keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,6 +1,7 @@
 #include "keyboard.h"
 #include "i8259.h"
 #include "lib.h"
+// An array to map scan codes to corresponding characters (layout-based)
 
 char keyboard[51] =
 {
@@ -10,18 +11,26 @@ char keyboard[51] =
     'S','D','F','G','H','J','K','L',';','\'',
     '`','s','\\', 'Z', 'X', 'C', 'V','B', 'N', 'M'
 };
+// Keyboard interrupt handler
 
 void keyboard_handler(){
 
-    cli();
-    uint8_t code = inb(0x60);
+    cli();// Disable interrupts to prevent interference during handling
+    uint8_t code = inb(0x60);    // Read scan code from the keyboard controller (PS/2)
+   
+    // Check if the code is within a valid range
+
     if(code < 52){
-        putc(keyboard[code]);
+        putc(keyboard[code]);        // Output the corresponding character to the screen
+
     }
-send_eoi(1);
-sti();
+send_eoi(1);    // Send an End of Interrupt (EOI) signal to the PIC controller for IRQ 1 (keyboard)
+
+sti();// Re-enable interrupts
 }
+// Function to initialize the keyboard
 
 void keyboard_init(){
-    enable_irq(1);
+    enable_irq(1);    // Enable IRQ 1 (keyboard interrupt) to allow keyboard input
+
 }
