27250a0f420803d03b1bf66e987b472e6da7bd2e
ankits6 <ankits6@illinois.edu>
2023-10-16 01:23:39 -0500

    Update file keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,23 +1,21 @@
-#include "keyboard.h"
 #include "i8259.h"
 #include "lib.h"
 
-char keyboard[51] = 
+char keyboard[51] =
 {
-  '\0','\0', '1','2','3','4', '5', '6', '7','8','9','0,'-','=','\0', '\0',
-  'q','w','e','r','t','y','u','i','o','p','[',']','\0', '\0',
-  'a','s','d','f','g','h','j','k','l',';','\'','`','\0','\\',
-  'z','x','c,'v','b','n','m'
+    NULL, 'e','1','2','3','4','5','6','7','8','9',
+    '0','-','=','b','t','Q','W','E','R','T',
+    'Y','U','I','O','P','[',']','e','l','A',
+    'S','D','F','G','H','J','K','L',';','\'',
+    '`','s','\\', 'Z', 'X', 'C', 'V','B', 'N', 'M'
 };
 
 void keyboard_handler(){
 
     cli();
-    
-    uint8_t scanline;
-    scanline = inb(0x60);
-    if(scanline < 52){
-        putc(keyboard[scanline]);
+    uint8_t code = inb(0x60);
+    if(code < 52){
+        putc(keyboard[code]);
     }
 send_eoi(1);
 sti();
