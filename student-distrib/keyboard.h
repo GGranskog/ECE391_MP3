2b685845aa66d59dd0098fb1b00f5f31ba029d6f
ankits6 <ankits6@illinois.edu>
2023-10-23 14:51:58 -0500

    Update keyboard.h

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -1,4 +1,3 @@
-
 /* keyboard.h - Defines used in interactions with the keyboard interrupt
  * controller
  */
@@ -19,6 +18,7 @@ int check_for_modifier(uint8_t scan_code);
 /* reads input from keyboard dataport when an interrupt occurs and put it to video memory */
 extern void keyboard_handler(void);
 
+int32_t keyboard_read (int32_t fd,  uint32_t offset, void* buf, int32_t nbytes)
 
 int32_t terminal_read(int32_t fd, uint32_t offset, void* buf, int32_t nbytes);
 /* write to the terminal with the contant in the buf of length */
