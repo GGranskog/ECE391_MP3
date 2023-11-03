9c7eafc66d0aa6685c1145da92376db5008cdd6e
Cardiel <acard6@illinois.edu>
2023-11-02 21:39:37 -0500

    updating

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -10,6 +10,25 @@
 #include "types.h"
 #include "filesys.h"
 
+typedef struct fop_table{
+    int32_t (*sys_open)(const uint8_t* fname);
+    int32_t (*sys_close)(int32_t fd);
+    int32_t (*sys_read)(int32_t fd, const void* buf, int32_t nbytes);
+    int32_t (*sys_write)(int32_t fd, const void* buf, int32_t nbytes);
+}fop_table_t;
+
+typedef struct fda_table{
+    fop_table_t* fop_table_ptr;
+    uint32_t inode;
+    uint32_t file_pos;
+    uint32_t flags;
+}fda_table_t;
+
+// fop_table_t open_fop;
+// fop_table_t close_fop;
+// fop_table_t read_fop;
+// fop_table_t write_fop;
+
 
 
 int32_t sys_exec(uint8_t* cmd);
