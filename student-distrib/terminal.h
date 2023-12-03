077d3862793cf6696113e790de2e7f7eb298b666
alan <acard6@illinois.edu>
2023-12-03 13:46:25 -0600

    terminal

---
+++ student-distrib/terminal.h
@@ -0,0 +1,19 @@
+#ifndef _TERMINAL_H
+#define _TERMINAL_H
+
+#include "multiboot.h"
+#include "x86_desc.h"
+#include "lib.h"
+#include "i8259.h"
+#include "tests.h"
+#include "idt.h"
+#include "types.h"
+#include "paging.h"
+#include "filesys.h"
+#include "keyboard.h"
+#include "rtc.h"
+#include "syscall.h"
+
+
+
+#endif
